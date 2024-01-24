3bd406002a95f7e681bd7f5bcc1ae45f64653bf6
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2024-01-24 09:25:15 -0300

    Update 4.2 - sqlAlchemyCoreApplicaion.py

--- 4.2 - sqlAlchemyCoreApplicaion.py
+++ 4.2 - sqlAlchemyCoreApplicaion.py
@@ -23,5 +23,31 @@ user_prefs = Table(
   Column('user_id', Integer, ForeignKey("user.user_id"), nullable=False,
   Column('pref_name', String(40), nullale+False),
   Column('pref_value', String(100)),
-
 )
+
+metadata_obj.creadte_all()
+
+for table in metadata_obj.sorted_tables:
+  print(table)
+
+metadata_db_obj = MetaData(schema='bank')
+  finacial_info = Table(
+    'finacial_info',
+  metadata_db_obj,
+  Column('id', Integer, primary_key=True),
+  Column('value', String(100), nullable=False),
+)
+
+print(\n"Info da tabela users")
+print(financial_info.primary_key)
+print(financial_info.constrains)
+
+# inserindo info na tabela user
+sql_insert = text('insert into user values(1, 'juliana', 'email@email.com',ju)')
+engine.execute(sql_insert)
+
+print('\nExecutando statement sql')
+sql = text('select * from user')
+result = engine.execute(sql)
+for row in result:
+  print(row)
