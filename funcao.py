1d4b86b9fc9719e70bfca7f2e0ce6f82090e7860
Bruna A. <130995251+moreandmorebr394@users.noreply.github.com>
2026-05-11 08:48:47 -0300

    Add function to check if a person is of legal age

---
+++ funcao.py
@@ -0,0 +1,9 @@
+def verificar_maioridade (idade = 18):
+    if idade >= 18:
+        print(f'Maior de idade')
+    else: 
+        idade <= 19
+        print(f'Menor de idade')
+verificar_maioridade(18)
+verificar_maioridade(14)
+verificar_maioridade(22)
